721d989a7ebaa7046b860b192b1fae15f4e16540
Antonio Jose Marin Rodriguez <antonio_josemr@hotmail.com>
2020-03-10 12:18:22 +0000

    heroku add

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const cool = require("cool-ascii-faces");
 const express = require("express");
 
 var app = express();
+var port = process.env.PORT || 80;
+
 app.use("/", express.static("./public"));
 app.get("/cool",(request,response) => {
 	response.send("<html>"+cool()+"</html>");
 });
 
-app.listen(80, () => {
+app.listen(port, () => {
 	console.log("Server ready");
 });
 
