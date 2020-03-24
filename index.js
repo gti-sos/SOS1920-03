a5846ba0a3ab20207a436a3895a3429e7f84d3ca
JuanRafaelBF <juanra91099@gmail.com>
2020-03-24 16:39:00 +0000

    index modificado

--- index.js
+++ index.js
@@ -7,79 +7,85 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 80;
 
-var contacts = [
+var exports_imports_stats = [
 	{ 
-		name: "peter",
-		phone: 123456	
+		country: "Alemania",
+		year: 2017,
+		import_profit: 1571000,
+		export_profit: 1104000,
+		food_export:5.61
 	},
 	{ 
-		name: "pablo",
-		phone: 789456	
+		country: "España",
+		year: 2017,
+		import_profit: 342400,
+		export_profit: 333400,
+		food_export: 16.23	
 	}
 ];
 
 const BASE_API_URL = "/api/v1";
 
-// GET CONTACTS
+// GET exports_imports_stats
 
-app.get(BASE_API_URL+"/contacts", (req,res) =>{
-	res.send(JSON.stringify(contacts,null,2));
-	console.log("Data sent:"+JSON.stringify(contacts,null,2));
+app.get(BASE_API_URL+"/exports_imports_stats", (req,res) =>{
+	res.send(JSON.stringify(exports_imports_stats,null,2));
+	console.log("Data sent:"+JSON.stringify(exports_imports_stats,null,2));
 });
 
 
-// POST CONTACTS
+// POST exports_imports_stats
 
-app.post(BASE_API_URL+"/contacts",(req,res) =>{
+app.post(BASE_API_URL+"/exports_imports_stats",(req,res) =>{
 	
-	var newContact = req.body;
+	var newStat = req.body;
 	
-	if((newContact == "") || (newContact.name == null)){
+	if((newStat == "") || (newStat.country == null)){
 		res.sendStatus(400,"BAD REQUEST");
 	} else {
-		contacts.push(newContact); 	
+		exports_imports_stats.push(newContact); 	
 		res.sendStatus(201,"CREATED");
 	}
 });
 
-// DELETE CONTACTS
+// DELETE || (newStat.country == null)
 
-// GET CONTACT/XXX
+// GET || (newStat.country == null)/country
 
-app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{
+app.get(BASE_API_URL+"/exports_imports_stats/:country", (req,res)=>{
 	
-	var name = req.params.name;
+	var country = req.params.country;
 	
-	var filteredContacts = contacts.filter((c) => {
-		return (c.name == name);
+	var filteredstats = exports_imports_stats.filter((c) => {
+		return (c.country == country);
 	});
 	
 	
-	if(filteredContacts.length >= 1){
-		res.send(filteredContacts[0]);
+	if(filteredstats.length >= 1){
+		res.send(filteredstats[0]);
 	}else{
-		res.sendStatus(404,"CONTACT NOT FOUND");
+		res.sendStatus(404,"COUNTRY NOT FOUND");
 	}
 });
 
 // PUT CONTACT/XXX
 
-// DELETE CONTACT/XXX
+// DELETE exports_imports_stats/country
 
-app.delete(BASE_API_URL+"/contacts/:name", (req,res)=>{
+app.delete(BASE_API_URL+"/exports_imports_stats/:country", (req,res)=>{
 	
-	var name = req.params.name;
+	var country = req.params.country;
 	
-	var filteredContacts = contacts.filter((c) => {
-		return (c.name != name);
+	var filteredstats = exports_imports_stats.filter((c) => {
+		return (c.country != country);
 	});
 	
 	
-	if(filteredContacts.length < contacts.length){
-		contacts = filteredContacts;
+	if(filteredstats.length < exports_imports_stats.length){
+		exports_imports_stats = filteredstats;
 		res.sendStatus(200);
 	}else{
-		res.sendStatus(404,"CONTACT NOT FOUND");
+		res.sendStatus(404,"COUNTRY NOT FOUND");
 	}
 	
 	
