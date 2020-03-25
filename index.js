870df2a2ac369f2786e6b03785b4615ec1a1b0a7
Antonio Jose Marin Rodriguez <antonio_josemr@hotmail.com>
2020-03-25 20:08:46 +0000

    f03 ajmr finalizado

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 80;
 
-var tourists_countries_stats = [
+var tourists_countries_stats = [];
+
+const BASE_API_URL = "/api/v1";
+
+//GET loadInitialData
+app.get(BASE_API_URL + "/tourists_countries_stats/loadInitialData", (req,res) => {
+	var stats= [
 	{ 
 		country: "Francia",
 		year: 2017,
@@ -16,22 +22,27 @@ var tourists_countries_stats = [
 		tourist_income:	60700000000
 	},
 	{ 
-		country: "España",
+		country: "Espana",
 		year: 2017,
 		tourist: 81900000,
 		difference_2016_17: 10.1,
 		tourist_income:	67900000000	
-	}
-];
-
-const BASE_API_URL = "/api/v1";
+	}];
+	tourists_countries_stats=stats;
+	res.send(JSON.stringify(stats,null,2))
+});
 
 // GET COUNTRIES
 
-app.get(BASE_API_URL+"/tourists_countries_stats/loadInitialData", (req,res) =>{
+app.get(BASE_API_URL+"/tourists_countries_stats", (req,res) =>{
 	res.send(JSON.stringify(tourists_countries_stats,null,2));
 	console.log("Data sent:"+JSON.stringify(tourists_countries_stats,null,2));
 });
+
+//PUT COUNTRIES ERROR
+app.put(BASE_API_URL+"/tourists_countries_stats", (req,res)=>{
+    res.status(405).send("NOT ALLOWED");
+})
 
 
 // POST COUNTRIES
@@ -49,6 +60,12 @@ app.post(BASE_API_URL+"/tourists_countries_stats",(req,res) =>{
 });
 
 // DELETE COUNTRIES
+
+app.delete(BASE_API_URL+"/tourists_countries_stats", (req,res)=>{
+    tourists_countries_stats = [];
+    res.send(JSON.stringify(tourists_countries_stats,null,2));
+    
+});
 
 // GET COUNTRY/XXX
 
@@ -70,6 +87,40 @@ app.get(BASE_API_URL+"/tourists_countries_stats/:country", (req,res)=>{
 
 // PUT COUNTRY/XXX
 
+app.put(BASE_API_URL +"/tourists_countries_stats/:country/:year",(req,res)=>{
+    var country=req.params.country;
+    var year=req.params.year;
+    var data=req.body;
+    
+    if(country!=data.country||year!=data.year){
+        res.status(400).send("BAD Request");
+    }else{
+        var filteredTCS = tourists_countries_stats.filter((c) => {
+        return (c.country != country || c.year != year);
+        });      
+        tourists_countries_stats = filteredTCS;
+        tourists_countries_stats.push(data);
+        res.status(200).send("DATA UPDATED");
+    }
+});
+
+//POST COUNTRY/xxx
+app.post(BASE_API_URL+"/tourists_countries_stats/:country", (req,res)=>{
+    res.status(405).send("NOT ALLOWED");
+})
+app.post(BASE_API_URL+"/tourists_countries_stats/:year", (req,res)=>{
+    res.status(405).send("NOT ALLOWED");
+})
+app.post(BASE_API_URL+"/tourists_countries_stats/:import_profit", (req,res)=>{
+    res.status(405).send("NOT ALLOWED");
+})
+app.post(BASE_API_URL+"/tourists_countries_stats/:export_profit", (req,res)=>{
+    res.status(405).send("NOT ALLOWED");
+})
+app.post(BASE_API_URL+"/tourists_countries_stats/:food_export", (req,res)=>{
+    res.status(405).send("NOT ALLOWED");
+})
+
 // DELETE COUNTRY/XXX
 
 app.delete(BASE_API_URL+"/tourists_countries_stats/:country", (req,res)=>{
@@ -85,7 +136,7 @@ app.delete(BASE_API_URL+"/tourists_countries_stats/:country", (req,res)=>{
 		tourists_countries_stats = filteredTCS;
 		res.sendStatus(200);
 	}else{
-		res.sendStatus(404,"COUNTRY NOT FOUND");
+		res.sendStatus(404,"NOT FOUND");
 	}
 	
 	
