1f75d710207baa1bda9895a8292825a86849eab0
JuanRafaelBF <juanra91099@gmail.com>
2020-04-13 17:49:48 +0000

    full nedb working

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser"); 	
 const path = require("path");
-const greetingAPI= require(path.join(__dirname,"greetingAPI_juabarfra"));
 const port = process.env.PORT || 80;
 const app = express();
 app.use(bodyParser.json());
+
+
+const greetingAPI= require(path.join(__dirname,"exports_imports_stats_API"));
 greetingAPI(app);
+
+
 
 app.listen(port, () => {
 	console.log("Server ready");
