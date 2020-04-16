5db8b21573aa9750638226b45afe3ca1e7a4ff40
Antonio Jose Marin Rodriguez <antonio_josemr@hotmail.com>
2020-04-16 12:01:51 +0000

    finalizado ajmr

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ app.use(bodyParser.json());
 //--------------------ANTONIO JOSE MARIN RODRIGUEZ------------------------------
 //------------------------------------------------------------------------------
 
-
-
+const TouristsCountriesStatsAPI = require(path.join(__dirname,"TouristsCountriesStatsAPI"));
+TouristsCountriesStatsAPI(app);
 
 //------------------------------------------------------------------------------
 //--------------------JUAN RAFAEL BARRAGAN FRANCO-------------------------------
 //------------------------------------------------------------------------------
+
 const exports_imports_stats_API= require(path.join(__dirname,"exports_imports_stats_API"));
 exports_imports_stats_API(app);
-
 
 //------------------------------------------------------------------------------
 //--------------------JOSE MANUEL GONZALEZ DOMINGUEZ----------------------------
 //------------------------------------------------------------------------------
+
 const public_budget_stats_API= require(path.join(__dirname,"public_budget_stats_API"));
 public_budget_stats_API(app);
+
 app.listen(port, () => {
 	console.log("Server ready");
 });
