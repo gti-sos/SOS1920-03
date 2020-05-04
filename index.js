b580cee78f6a4492028370de681118d20405a1ac
JuanRafaelBF <juanra91099@gmail.com>
2020-05-04 13:24:27 +0200

    hay fallos

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser"); 	
-const path = require("path");
+
+const back = require("./src/back");
 const port = process.env.PORT || 80;
 const app = express();
 app.use(bodyParser.json());
 
+back(app);
+
+
 app.use("/", express.static("./public"));
 
-//------------------------------------------------------------------------------
-//--------------------ANTONIO JOSE MARIN RODRIGUEZ------------------------------
-//------------------------------------------------------------------------------
 
-const tourists_countries_stats_API = require(path.join(__dirname,"tourists_countries_stats_API"));
-tourists_countries_stats_API(app);
-
-//------------------------------------------------------------------------------
-//--------------------JUAN RAFAEL BARRAGAN FRANCO-------------------------------
-//------------------------------------------------------------------------------
-
-const exports_imports_stats_API= require(path.join(__dirname,"exports_imports_stats_API"));
-exports_imports_stats_API(app);
-
-//------------------------------------------------------------------------------
-//--------------------JOSE MANUEL GONZALEZ DOMINGUEZ----------------------------
-//------------------------------------------------------------------------------
-
-const public_budget_stats_API= require(path.join(__dirname,"public_budget_stats_API"));
-public_budget_stats_API(app);
 
 app.listen(port, () => {
 	console.log("Server ready");
 });
 
-console.log("Starting server...");
+console.log("Starting server on port "+port);
 
---
+++ src/front/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import export_import_stats_Table from "./export_import_stats_Table.svelte";
+</script>
+
+<main>
+	<export_import_stats_Table />
+</main>
+
